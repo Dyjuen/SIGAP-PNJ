95ccca1887d8df20a57c35f78b8a827af069438e
Elmanda1 <falihelmanda@gmail.com>
2025-12-18 10:09:33 +0700

    Revert "fix enhance flasher"
    This reverts commit 7da74062690293c8e13e42d2ff86a3ef674d5d2f.

--- public/src/js/components/FlasherNotification.js
+++ public/src/js/components/FlasherNotification.js
@@ -126,7 +126,7 @@ export class FlasherNotification {
       background: rgba(255, 255, 255, 0.2);
       border-radius: 50%;
       display: flex;
-      align-items: flex-end;
+      align-items: center;
       justify-content: center;
       font-size: 24px;
       flex-shrink: 0;
